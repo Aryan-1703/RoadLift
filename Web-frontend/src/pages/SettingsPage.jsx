59b7382c567ab2c1a7f5d3e392f0558abde513ad
Aryan <aryan17032@gmail.com>
2026-02-16 19:50:15 -0500

    update to latest sdk 54

--- Web-frontend/src/pages/SettingsPage.jsx
+++ Web-frontend/src/pages/SettingsPage.jsx
@@ -1,5 +1,5 @@
 import React, { useContext } from "react";
-import ThemeContext from "../_context/ThemeContext";
+import ThemeContext from "../context/ThemeContext";
 import { FaSun, FaMoon } from "react-icons/fa";
 
 const SettingsPage = () => {
--- frontend/app/config/constants.js
+++ frontend/app/config/constants.js
@@ -1,5 +1,5 @@
-export const API_URL = "http://10.0.0.125:8001/api";
-export const API_BASE_URL = "http://10.0.0.125:8001";
+export const API_URL = "http://72.143.199.139:8001/api";
+export const API_BASE_URL = "http://72.143.199.139:8001";
 // export const API_URL = "http://172.20.10.7:8001/api";
 // export const API_BASE_URL = "http://172.20.10.7:8001";
 
